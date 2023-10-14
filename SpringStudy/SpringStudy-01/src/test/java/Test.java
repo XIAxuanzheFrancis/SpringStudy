0b0d29daf635fd7e26d5974cdd48bf16c7d9780a
xiaxuanzhe <3058634081@qq.com>
2023-10-14 13:16:51 +0200

    commencer à écrire le fichier.xml-->IoC

--- SpringStudy/SpringStudy-01/src/test/java/Test.java
+++ SpringStudy/SpringStudy-01/src/test/java/Test.java
@@ -3,12 +3,17 @@ import com.xuanzhe.dao.UserDaoMysqlImpl;
 import com.xuanzhe.dao.UserDaoOracleImpl;
 import com.xuanzhe.service.UserService;
 import com.xuanzhe.service.UserServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
   public static void main(String[] args) {
-    UserService userService = new UserServiceImpl();
+   /* UserService userService = new UserServiceImpl();
 
     ((UserServiceImpl)userService).setUserDao(new UserDaoOracleImpl());
-    userService.getUser();
+    userService.getUser();*/
+    ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+    UserServiceImpl userServiceImpl = (UserServiceImpl) context.getBean("userServiceImpl");
+    userServiceImpl.getUser();
   }
 }
