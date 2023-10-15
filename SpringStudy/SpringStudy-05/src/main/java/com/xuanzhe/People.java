fd5cbe711ca4baa9f17decda91b231a97a9b4347
xiaxuanzhe <3058634081@qq.com>
2023-10-15 20:55:26 +0200

    autowired

--- SpringStudy/SpringStudy-05/src/main/java/com/xuanzhe/People.java
+++ SpringStudy/SpringStudy-05/src/main/java/com/xuanzhe/People.java
@@ -1,8 +1,16 @@
 package com.xuanzhe;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+
 public class People {
   private String name;
+  @Autowired
+  @Qualifier(value="cat111")
   private Cat cat;
+  @Autowired
+  @Qualifier(value="dog111")
   private Dog dog;
 
   public String getName() {
