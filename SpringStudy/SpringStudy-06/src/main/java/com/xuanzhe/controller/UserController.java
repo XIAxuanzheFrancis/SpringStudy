6abb698e75c1ad7b0a1b1c7703d23e0b18241c57
xiaxuanzhe <3058634081@qq.com>
2023-10-15 21:25:35 +0200

    Développement avec des annotations

---
+++ SpringStudy/SpringStudy-06/src/main/java/com/xuanzhe/controller/UserController.java
@@ -0,0 +1,8 @@
+package com.xuanzhe.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+}
--- SpringStudy/SpringStudy-06/src/main/java/com/xuanzhe/pojo/User.java
+++ SpringStudy/SpringStudy-06/src/main/java/com/xuanzhe/pojo/User.java
@@ -1,8 +1,10 @@
 package com.xuanzhe.pojo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class User {
-  public String name = "xiaxuanzhe";
+  @Value("xiaxuanzhe")
+  public String name;
 }
